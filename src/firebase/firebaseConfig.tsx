acde461006dd2bd7012760144d3a881c1a139126
dev8 <yuka@MacBook-Air.local>
2024-06-23 01:18:15 +0900

    update env

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -1,16 +1,15 @@
-// firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9XUdyFTIrhtb1XgAmhXD-UNhHu1pNgg8",
-    authDomain: "memogo-fb41e.firebaseapp.com",
-    projectId: "memogo-fb41e",
-    storageBucket: "memogo-fb41e.appspot.com",
-    messagingSenderId: "1071063746596",
-    appId: "1:1071063746596:web:1567a086247576d53cf0b1"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
