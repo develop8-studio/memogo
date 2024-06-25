ce2df5f77c28c87e45234a1296c14b52eb5068c0
dev8 <yuka@MacBook-Air.local>
2024-06-26 00:35:01 +0900

    update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import Link from "next/link";
+import React, { useState, useEffect } from 'react';
+import { FaHome, FaUser, FaCog, FaSignOutAlt, FaBookmark, FaPen, FaUserFriends, FaFire } from 'react-icons/fa';
+import Link from 'next/link';
 import { auth } from '@/firebase/firebaseConfig';
 import { signOut, onAuthStateChanged, User } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import { Button, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay } from '@chakra-ui/react';
-import { FaHome, FaUser, FaCog, FaSignOutAlt, FaBookmark, FaPen, FaUserFriends, FaFire } from 'react-icons/fa';
 
 interface MenuItemProps {
     icon: React.ReactNode;
-    text: string;
     href?: string; // href is optional
     onClick?: () => void; // Optional onClick handler
 }
 
-const Navbar: React.FC = () => {
+const SideBar: React.FC = () => {
     const router = useRouter();
-    const [isOpen, setIsOpen] = useState(false);
     const [user, setUser] = useState<User | null>(null);
     const [isAlertOpen, setIsAlertOpen] = useState(false);
     const onClose = () => setIsAlertOpen(false);
@@ -34,10 +31,6 @@ const Navbar: React.FC = () => {
         return () => unsubscribe();
     }, []);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
     const handleLogout = async () => {
         setIsAlertOpen(false);
         await signOut(auth);
@@ -48,43 +41,21 @@ const Navbar: React.FC = () => {
         setIsAlertOpen(true);
     };
 
-    const menuItems: MenuItemProps[] = [
-        { icon: <FaHome className="text-lg" />, text: "ホーム", href: "/" },
-        { icon: <FaFire className="text-lg" />, text: "フィード", href: "/feed" },
-        { icon: <FaUser className="text-lg" />, text: "プロフィール", href: user ? `/user?id=${user.uid}` : undefined },
-        { icon: <FaUserFriends className="text-lg" />, text: "フォロー中", href: "/following" },
-        { icon: <FaBookmark className="text-lg" />, text: "ブックマーク", href: "/bookmarks" },
-        { icon: <FaPen className="text-lg" />, text: "エディター", href: "/editor" },
-        { icon: <FaCog className="text-lg" />, text: "設定", href: "/settings" },
-        { icon: <FaSignOutAlt className="text-lg" />, text: "ログアウト", onClick: handleLogoutClick },
-    ];
-
     return (
         <>
-            <div className="fixed top-0 w-full bg-teal-500 text-white border-b border-teal-500 z-50 md:hidden">
-                <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
-                    <Link href="/">
-                        <a className="font-bold">
-                            ロゴ
-                        </a>
-                    </Link>
-                    <button
-                        onClick={toggleMenu}
-                        className="p-2 md:hidden"
-                        aria-label="Toggle Navigation"
-                    >
-                        {isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                    </button>
+            <div className="hidden md:flex sticky top-0 h-screen w-16 flex-col bg-gray-800 text-white shadow-sm">
+                <div className="flex-grow">
+                    <MenuItem icon={<FaHome className="text-lg" />} href="/" />
+                    <MenuItem icon={<FaFire className="text-lg" />} href="/feed" />
+                    {user && <MenuItem icon={<FaUser className="text-lg" />} href={`/user?id=${user.uid}`} />}
+                    <MenuItem icon={<FaUserFriends className="text-lg" />} href="/following" />
+                    <MenuItem icon={<FaBookmark className="text-lg" />} href="/bookmarks" />
+                    <MenuItem icon={<FaPen className="text-lg" />} href="/editor" />
+                    <MenuItem icon={<FaCog className="text-lg" />} href="/settings" />
                 </div>
-                {isOpen && (
-                    <div className="md:hidden">
-                        <div className="bg-teal-500 px-4 pt-2 pb-4">
-                            {menuItems.map((item, index) => (
-                                <MenuItem key={index} icon={item.icon} text={item.text} href={item.href} onClick={item.onClick} />
-                            ))}
-                        </div>
-                    </div>
-                )}
+                <div>
+                    <MenuItem icon={<FaSignOutAlt className="text-lg" />} onClick={handleLogoutClick} />
+                </div>
             </div>
 
             <AlertDialog
@@ -96,19 +67,19 @@ const Navbar: React.FC = () => {
                 <AlertDialogOverlay>
                     <AlertDialogContent>
                         <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                            ログアウト
+                            Logout
                         </AlertDialogHeader>
 
                         <AlertDialogBody>
-                            ログアウトしてもよろしいですか？
+                            Are you sure you want to log out?
                         </AlertDialogBody>
 
                         <AlertDialogFooter>
                             <Button ref={cancelRef} onClick={onClose}>
-                                キャンセル
+                                Cancel
                             </Button>
                             <Button colorScheme="blue" onClick={handleLogout} ml={3}>
-                                ログアウト
+                                Logout
                             </Button>
                         </AlertDialogFooter>
                     </AlertDialogContent>
@@ -118,24 +89,20 @@ const Navbar: React.FC = () => {
     );
 };
 
-const MenuItem: React.FC<MenuItemProps> = ({ icon, text, href, onClick }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ icon, href, onClick }) => {
     if (href) {
         return (
-            <Link href={href} passHref>
-                <a className="block py-2 flex items-center">
-                    {icon}
-                    <span className="ml-2">{text}</span>
-                </a>
+            <Link href={href} className="h-16 flex items-center justify-center transition-colors hover:bg-gray-500 rounded-tl-md rounded-bl-md">
+                {icon}
             </Link>
         );
     } else {
         return (
-            <button className="block py-2 flex items-center" onClick={onClick}>
+            <button className="h-16 w-full flex items-center justify-center transition-colors hover:bg-gray-500 rounded-tl-md rounded-bl-md" onClick={onClick}>
                 {icon}
-                <span className="ml-2">{text}</span>
             </button>
         );
     }
 };
 
-export default Navbar;
+export default SideBar;
